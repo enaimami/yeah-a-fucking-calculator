15ce3489966dfb559fa151ea3352d27cb5024d62
mami <1terabyterma@gmail.com>
2025-12-04 06:55:57 +0300

    i added argument parsing or something like that

--- main.c
+++ main.c
@@ -11,6 +11,27 @@ int	m_strlen(char *str)
 	}
 	return (i);
 }
+int	m_array_strlen(int size,int first, char **str_arr)
+{
+	int	total;
+	int	i;
+	int j;
+
+	total = 0;
+	i = first;
+	while(i < size)
+	{
+		j = 0;
+		while(str_arr[i][j] != '\0')
+		{
+			total++;
+			j++;
+		}
+		i++;
+	}
+	return (total);
+}
+
 int	space_count(char *formula)
 {
 	int	i;
@@ -79,10 +100,40 @@ char	solve(char *formula)
 		
 	}
 }
-
-
-int	main(int ac,char  **argv)
+char	*concatenate_arguments(int ac,char **av)
 {
-	printf("%s", skip_spaces(argv[1]));
+	int	i;
+	int	j;
+	int	formula_len;
+	char *formula;
+
+	i = 1;
+	formula_len = 0;
+	formula = (char*)malloc(m_array_strlen(ac,1,av) + 1);
+	while (i < ac)
+	{
+		j = 0;
+		while(av[i][j] != '\0')
+		{
+			formula[formula_len] = av[i][j];
+			formula_len++;
+			j++;
+		}
+		i++;
+	}
+	formula[formula_len] = '\0';
+	return (formula);
+}
+
+int	main(int ac,char  **av)
+{
+	if(ac >= 1)
+	{
+		printf("%s",m_skip_extras(concatenate_arguments(ac,av)));
+	}
+	else
+	{
+
+	}
 	
 }
